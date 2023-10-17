bf6bac37c07ef9b3ee01004ff447519b26f29c77
knowsnobody101 <148236285+knowsnobody101@users.noreply.github.com>
2023-10-17 08:53:23 -0700

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='flex'>
+          <img src="/image.png" className="logo" alt="Vite logo" />
+      </div>
+    </>
+  )
+}
+
+export default App
